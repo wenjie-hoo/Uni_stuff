f559f532c78782834c3ae5f3bc762c20c4054ee2
Clark <clark0712@foxmail.com>
2023-11-16 10:29:49 +0100

    c++ lab4 p3

--- C++&STL/lab4/p3.cpp
+++ C++&STL/lab4/p3.cpp
@@ -11,18 +11,136 @@ For each task, define a function object (with internal state) and use it in a fo
 do not use accumulate() or similar functions.
 */
 
-#include<iostream>
-#include<vector>
-#include<list>
-#include<set>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-vector < double > v;
-list < string > l;
-set < int > s;
+// Functor to print values in the range (greater than a and less than b)
+template<typename T>
+class PrintInRange {
+private:
+    T a, b;
+public:
+    PrintInRange(T a, T b) : a(a), b(b) {}
+    void operator()(const T& elem) const {
+        if (elem > a && elem < b) {
+            cout << elem << " ";
+        }
+    }
+};
 
-void fill(){}
+// Functor to output every kth value starting from the p-th position
+template<typename T>
+class OutputEveryKth {
+private:
+    int p, k;
+    int position = 0;
 
-int main(){
+public:
+    OutputEveryKth(int p, int k) : p(p), k(k) {}
+
+    void operator()(const T& elem) {
+        if (position >= p && (position - p) % k == 0) {
+            cout << elem << " ";
+        }
+        position++;
+    }
+};
+
+// Functor to determine the arithmetic mean
+template<typename T>
+class CalculateArithmeticMean {
+private:
+    T sum = 0;
+    int count = 0;
+
+public:
+    void operator()(const T& elem) {
+        sum += elem;
+        count++;
+    }
+
+    double getMean() const {
+        return count > 0 ? static_cast<double>(sum) / count : 0.0;
+    }
+};
+
+// Functor to determine the minimum and maximum element
+template<typename T>
+class FindMinMax {
+private:
+    T minElem, maxElem;
+    bool firstElement = true;
+
+public:
+    void operator()(const T& elem) {
+        if (firstElement) {
+            minElem = elem;
+            maxElem = elem;
+            firstElement = false;
+        } else {
+            minElem = min(minElem, elem);
+            maxElem = max(maxElem, elem);
+        }
+    }
+
+    pair<T, T> getMinMax() const {
+        return make_pair(minElem, maxElem);
+    }
+};
+
+// Functor to determine the sum (or concatenation for character strings)
+template<typename T>
+class CalculateSum {
+private:
+    T sum;
+
+public:
+    CalculateSum() : sum() {}
+
+    void operator()(const T& elem) {
+        sum += elem;
+    }
+
+    T getSum() const {
+        return sum;
+    }
+};
+
+int main() {
+    vector<double> Tvect{1.2, 2.3, 3.4, 4.5, 5.6, 6.7};
+    list<string> Tlist{"a", "b", "c", "d", "e", "f", "g", "h"};
+    set<int> Tset{1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+
+    cout << "print in range" << endl;
+    PrintInRange<double> printInRange(3, 5);
+    for_each(Tvect.begin(), Tvect.end(), printInRange);
+    
+
+    OutputEveryKth<string> outputEveryKth(3, 2);
+    for_each(Tlist.begin(), Tlist.end(), outputEveryKth);
+    cout << endl;
+
+    CalculateArithmeticMean<int> calculateMean;
+    for_each(Tset.begin(), Tset.end(), calculateMean);
+    cout << "Arithmetic Mean: " << calculateMean.getMean() << endl;
+
+    // Task 4: Determine the minimum and maximum element
+    FindMinMax<double> findMinMax;
+    for_each(Tvect.begin(), Tvect.end(), findMinMax);
+    auto minMaxPair = findMinMax.getMinMax();
+    cout << "Min: " << minMaxPair.first << ", Max: " << minMaxPair.second << endl;
+
+    // Task 5: Determine the sum (concatenation for character strings)
+    CalculateSum<string> calculateStringSum;
+    for_each(Tlist.begin(), Tlist.end(), calculateStringSum);
+    cout << "Concatenated String: " << calculateStringSum.getSum() << endl;
+
+    return 0;
 }
